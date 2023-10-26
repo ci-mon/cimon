0cf6794110148dfc93ef16ef244668641934c090
Vadym Artemchuk <v.artemchuk@creatio.com>
2023-10-26 21:55:01 +0300

    build info refactorings and build info view improvements

---
+++ src/Cimon.Data.TeamCity.Tests/BaseTeamCityTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Cimon.Data.TeamCity.Tests;
+
+public class BaseTeamCityTest
+{
+	protected ServiceProvider ServiceProvider = null!;
+
+	[SetUp]
+	public virtual void Setup() {
+		var secrets = new TeamcitySecrets {
+			Uri = new Uri("****"),
+			Login = "****",
+			Password = "****"
+		};
+		ServiceProvider = new ServiceCollection()
+			.AddSingleton(Options.Create(secrets))
+			.AddCimonDataTeamCity()
+			.AddLogging()
+			.BuildServiceProvider();
+	}
+}
