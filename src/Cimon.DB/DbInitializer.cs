5a99fe9e09d755fb43f116a0e50734fe7c28905f
Vadym Artemchuk <vadimart92@gmail.com>
2023-10-27 23:50:02 +0300

    build info view improved

--- src/Cimon.DB/DbInitializer.cs
+++ src/Cimon.DB/DbInitializer.cs
@@ -176,7 +176,7 @@ public class DbInitializer
 				StartDate = DateTime.Now.AddHours(-1),
 				BranchName = "trunk",
 				Changes = Enumerable.Range(0, 10).Select(i => 
-						new VcsChange(new VcsUser($"test{i}", "Test {i}", "milton.soto@example.com"), DateTimeOffset.Now, string.Empty, ImmutableArray<FileModification>.Empty))
+						new VcsChange(new VcsUser($"test{i}", $"Test {i}", "milton.soto@example.com"), DateTimeOffset.Now, string.Empty, ImmutableArray<FileModification>.Empty))
 					.Prepend(new VcsChange(new VcsUser("admin", "Admin", "bedete.araujo@example.com"), DateTimeOffset.Now, string.Empty, ImmutableArray<FileModification>.Empty))
 					.ToArray(),
 				BuildConfigId = "app.studio-enterprise.shell",
