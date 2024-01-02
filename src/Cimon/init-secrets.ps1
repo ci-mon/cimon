11be5ca50f14fa5616f0d48666c8520adb712813
Vadym Artemchuk <vadimart92@gmail.com>
2024-01-02 23:09:18 +0200

    better example

--- src/Cimon/init-secrets.ps1
+++ src/Cimon/init-secrets.ps1
@@ -2,7 +2,7 @@
 type .\secrets.local.json | dotnet user-secrets set
 
 # prod
-#setx CIMON_Secrets__Vault__Url "http://example:8200" /M
-#setx CIMON_Secrets__Vault__Token "example" /M
+#setx CIMON_Secrets__Vault__Url "http://localhost:8200" /M
+#setx CIMON_Secrets__Vault__Token "root" /M
 #setx CIMON_Secrets__Vault__Path "dev" /M
 
