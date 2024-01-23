bdce78d72eb8e16ac64f1916ca3ed171b9b036fd
Vadym Artemchuk <v.artemchuk@creatio.com>
2024-01-23 19:10:46 +0200

    ui improvements

--- src/Cimon.Data/Users/UserSupervisorActor.cs
+++ src/Cimon.Data/Users/UserSupervisorActor.cs
@@ -3,10 +3,16 @@ using Cimon.Data.Common;
 
 namespace Cimon.Data.Users;
 
+using Microsoft.Extensions.Logging;
+
 class UserSupervisorActor : ReceiveActor
 {
-    public UserSupervisorActor() {
+    public UserSupervisorActor(ILogger<UserSupervisorActor> logger) {
         Receive<ActorsApi.UserMessage>(msg => {
+            if (string.IsNullOrWhiteSpace(msg.UserName)) {
+                logger.LogDebug("Message to empty user {Message}", msg);
+                return;
+            }
             var child = Context.GetOrCreateChild<UserActor>(msg.UserName);
             child.Forward(msg);
         });
