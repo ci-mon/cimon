da6fd5515419a0e0d2ac495462097b61ef2b5d7c
Vadym Artemchuk <vadimart92@gmail.com>
2024-04-11 18:50:18 +0300

    added lost migration

---
+++ src/Cimon.DB.Migrations.SqlServer/Migrations/20240411154825_monitor_order.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cimon.DB.Migrations.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class monitor_order : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ViewSettings",
+                table: "Monitors",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ViewSettings",
+                table: "Monitors");
+        }
+    }
+}
