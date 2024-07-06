1da98ccc4adcba57024c9e7255f9c2271f91f680
Vadym Artemchuk <vadimart92@gmail.com>
2024-07-06 23:48:49 +0300

    ignore pool stopping errors during deploy

--- src/build/Program.cs
+++ src/build/Program.cs
@@ -14,112 +14,103 @@ namespace Build;
 
 public static class Program
 {
-    public static int Main(string[] args)
-    {
-        return new CakeHost()
-            .UseContext<BuildContext>()
-            .Run(args);
-    }
+	public static int Main(string[] args) {
+		return new CakeHost().UseContext<BuildContext>().Run(args);
+	}
 }
 
 public class BuildContext : FrostingContext
 {
-    public string AppPoolName { get; set; } = "cimon";
+	public string AppPoolName { get; set; } = "cimon";
 
-    public ConvertableDirectoryPath CimonProject { get; set; }
+	public ConvertableDirectoryPath CimonProject { get; set; }
 
-    public DirectoryPath PublishDir { get; set; }
+	public DirectoryPath PublishDir { get; set; }
 
-    public BuildContext(ICakeContext context)
-        : base(context)
-    {
-        CimonProject = context.Directory("../Cimon/Cimon.csproj");
-        PublishDir = context.Argument("publish-dir", "../../output/cimon");
-    }
+	public BuildContext(ICakeContext context) : base(context) {
+		CimonProject = context.Directory("../Cimon/Cimon.csproj");
+		PublishDir = context.Argument("publish-dir", "../../output/cimon");
+	}
 }
 
 [TaskName("Publish")]
 public sealed class PublishTask : FrostingTask<BuildContext>
 {
-    public override void Run(BuildContext context) {
-        context.DotNetPublish(context.CimonProject, new DotNetPublishSettings {
-            Configuration = "Release",
-            Runtime = "win-x64",
-            SelfContained = true,
-            OutputDirectory = context.PublishDir
-        });
-    }
+	public override void Run(BuildContext context) {
+		context.DotNetPublish(context.CimonProject, new DotNetPublishSettings {
+			Configuration = "Release",
+			Runtime = "win-x64",
+			SelfContained = true,
+			OutputDirectory = context.PublishDir
+		});
+	}
 }
 
 [TaskName("Deploy")]
 [IsDependentOn(typeof(PublishTask))]
 public sealed class DeployTask : FrostingTask<BuildContext>
 {
-    public override void Run(BuildContext context) {
-        var deployServerName = context.Argument<string>("deploy-server-name");
-        var deployPath = context.Argument<string>("deploy-path");
-        var ci = context.Argument("ci", true);
-        var stopPoolScript =
-            $$"""
-                Stop-WebAppPool -Name "{{context.AppPoolName}}" -ErrorAction SilentlyContinue;
-                $WorkerProcesses = & "$env:SystemRoot\system32\inetsrv\appcmd.exe" list wp
-                $pattern = 'WP "(\d+)" \(applicationPool:{{context.AppPoolName}}\)'
-                $match = [regex]::Match($WorkerProcesses, $pattern)
-                if ($match.Success) {
-                    $ProcessId = $match.Groups[1].Value
-                    Write-Host "Killing process ID: $ProcessId"
-                    # Kill the process
-                    $counter = 15;
-                    Write-Host "Waiting for process to stop"
-                    $processStopped = $false
-                    while ($counter -gt 0) {
-                        $process = Get-Process -Id $ProcessId -ErrorAction SilentlyContinue
-                        if ($process -eq $null) {
-                            $processStopped = $true
-                            break
-                        }
-                        Start-Sleep 1
-                        $counter--;
-                    }
-                    if (-not $processStopped) {
-                        Write-Host "Process did not stop in time, killing it"
-                        Stop-Process -Id $ProcessId -Force
-                    }
-                }
-            """;
-        context.StartPowershellScript(stopPoolScript, new PowershellSettings {
-            ComputerName = deployServerName,
-            Modules = new[]{"webadministration"},
-            ExceptionOnScriptError = false,
-            OutputToAppConsole = !ci,
-            LogOutput = ci
-        });
-        context.Information("Synchronizing files");
-        var webConfig = "web.config";
-        context.StartProcess("robocopy", new ProcessSettings {
-            Arguments = new ProcessArgumentBuilder()
-                .AppendQuoted(context.PublishDir.FullPath)
-                .AppendQuoted(deployPath)
-                .Append("/MIR")
-                .Append("/R:15")
-                .Append("/W:5")
-                .Append("/XD").AppendQuoted("nativeApps").AppendQuoted("db").AppendQuoted("logs")
-                .Append("/XF").AppendQuoted(webConfig)
-                .Append("/XF").AppendQuoted("appsettings.Production.json")
-        });
-        var webConfigPath = context.File(webConfig);
-        var webConfigDestination = context.Directory(deployPath) + webConfigPath;
-        if (!context.FileExists(webConfigDestination)) {
-             context.CopyFile(context.PublishDir.GetFilePath(webConfig), webConfigDestination);
-        }
-        context.StartPowershellScript($"Start-WebAppPool -Name \"{context.AppPoolName}\"", new PowershellSettings {
-            ComputerName = deployServerName,
-            Modules = new[]{"webadministration"},
-            OutputToAppConsole = !ci,
-            LogOutput = ci
-        });
-        context.Log.Information("Cimon app deployed");
-    }
+	public override void Run(BuildContext context) {
+		var deployServerName = context.Argument<string>("deploy-server-name");
+		var deployPath = context.Argument<string>("deploy-path");
+		var ci = context.Argument("ci", true);
+		var stopPoolScript = $$"""
+		Stop-WebAppPool -Name "{{context.AppPoolName}}" -ErrorAction SilentlyContinue;
+		$WorkerProcesses = & "$env:SystemRoot\system32\inetsrv\appcmd.exe" list wp
+		if ($WorkerProcesses){
+			$pattern = 'WP "(\d+)" \(applicationPool:{{context.AppPoolName}}\)'
+			$match = [regex]::Match($WorkerProcesses, $pattern)
+			if ($match.Success) {
+				$ProcessId = $match.Groups[1].Value
+				Write-Host "Killing process ID: $ProcessId"
+				# Kill the process
+				$counter = 15;
+				Write-Host "Waiting for process to stop"
+				$processStopped = $false
+				while ($counter -gt 0) {
+					$process = Get-Process -Id $ProcessId -ErrorAction SilentlyContinue
+					if ($process -eq $null) {
+						$processStopped = $true
+						break
+					}
+					Start-Sleep 1
+					$counter--;
+				}
+				if (-not $processStopped) {
+					Write-Host "Process did not stop in time, killing it"
+					Stop-Process -Id $ProcessId -Force
+				}
+			}
+		}
+		""";
+		context.StartPowershellScript(stopPoolScript, new PowershellSettings {
+			ComputerName = deployServerName,
+			Modules = new[] { "webadministration" },
+			ExceptionOnScriptError = false,
+			OutputToAppConsole = !ci,
+			LogOutput = ci
+		});
+		context.Information("Synchronizing files");
+		var webConfig = "web.config";
+		context.StartProcess("robocopy", new ProcessSettings {
+			Arguments = new ProcessArgumentBuilder().AppendQuoted(context.PublishDir.FullPath).AppendQuoted(deployPath)
+				.Append("/MIR").Append("/R:15").Append("/W:5").Append("/XD").AppendQuoted("nativeApps")
+				.AppendQuoted("db").AppendQuoted("logs").Append("/XF").AppendQuoted(webConfig).Append("/XF")
+				.AppendQuoted("appsettings.Production.json")
+		});
+		var webConfigPath = context.File(webConfig);
+		var webConfigDestination = context.Directory(deployPath) + webConfigPath;
+		if (!context.FileExists(webConfigDestination)) {
+			context.CopyFile(context.PublishDir.GetFilePath(webConfig), webConfigDestination);
+		}
+		context.StartPowershellScript($"Start-WebAppPool -Name \"{context.AppPoolName}\"", new PowershellSettings {
+			ComputerName = deployServerName,
+			Modules = new[] { "webadministration" },
+			OutputToAppConsole = !ci,
+			LogOutput = ci
+		});
+		context.Log.Information("Cimon app deployed");
+	}
 }
 
 [TaskName("Default")]
